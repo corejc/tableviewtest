d5e403b0b78a7b6d2fb32a7f0cb6fedff8dce50e
corejc <corejc@me.com>
2017-06-22 16:53:32 +0900

    testing

--- hidekeyboard/ViewController.swift
+++ hidekeyboard/ViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    override func viewDidLoad() {
+    var test : String = ""
+    
+    
+   override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
